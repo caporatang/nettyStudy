d4e1261d7d7f1e3f01ec8917be2a12ac0b9b2ece
caporatang <caporatang@naver.com>
2023-08-30 22:47:27 +0900

    docs : 3.3.1 ServerBootstrap API ChannelFactory

---
+++ src/main/java/com/example/nettystudy/netty/chapter3/bootstrapApi/ChannelFactory.java
@@ -0,0 +1,20 @@
+package com.example.nettystudy.netty.chapter3.bootstrapApi;
+
+/**
+ * packageName : com.example.nettystudy.netty.chapter3.bootstrapApi
+ * fileName : ChannelFactory
+ * author : taeil
+ * date : 2023/08/30
+ * description :
+ * =======================================================
+ * DATE          AUTHOR                      NOTE
+ * -------------------------------------------------------
+ * 2023/08/30        taeil                   최초생성
+ */
+public class ChannelFactory {
+    // 소켓의 입출력 모드를 설정하는 API
+
+    // channelFactory 메서드는 channel 메서드와 동일하게 소켓의 입출력 모드를 설정하는 API
+    // ChannelFactory 인터페이스를 상속받은 클래스를 설정할 수 있으며 channel 메서드와 동일한 기능을 수행.
+    // 네티가 제공하는 ChannelFactory 인터페이스의 구현체로는 NioUdtProvider가 있다.
+}
