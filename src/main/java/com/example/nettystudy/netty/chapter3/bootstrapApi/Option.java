484708c4615d6010de78bc32248ca9922d699a44
caporatang <caporatang@naver.com>
2023-08-31 23:08:04 +0900

    Feat : 3.3.1 ServerBootstrap API Option

---
+++ src/main/java/com/example/nettystudy/netty/chapter3/bootstrapApi/Option.java
@@ -0,0 +1,19 @@
+package com.example.nettystudy.netty.chapter3.bootstrapApi;
+
+/**
+ * packageName : com.example.nettystudy.netty.chapter3.bootstrapApi
+ * fileName : Option
+ * author : taeil
+ * date : 2023/08/31
+ * description :
+ * =======================================================
+ * DATE          AUTHOR                      NOTE
+ * -------------------------------------------------------
+ * 2023/08/31        taeil                   최초생성
+ */
+public class Option {
+    // 클라이언트 소켓 채널의 소켓 옵션을 설정
+
+    // ServerBootstrap의 option 메서드가 서버 소켓 채널의 옵션을 설정한 반면
+    // Bootstrap의 option 메서드는 서버와 연결된 클라이언트 소켓 채널의 옵션을 설정한다.
+}
