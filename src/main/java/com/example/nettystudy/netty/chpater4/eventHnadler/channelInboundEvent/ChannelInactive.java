c368c7a3b5593277c08b80254cf451b4179dbdec
caporatang <caporatang@naver.com>
2023-09-02 22:40:02 +0900

    Docs : 4.3.1 channelInactive, channelUnregistered

---
+++ src/main/java/com/example/nettystudy/netty/chpater4/eventHnadler/channelInboundEvent/ChannelInactive.java
@@ -0,0 +1,17 @@
+package com.example.nettystudy.netty.chpater4.eventHnadler.channelInboundEvent;
+
+/**
+ * packageName : com.example.nettystudy.netty.chpater4.eventHnadler.channelInboundEvent
+ * fileName : ChannelInactive
+ * author : taeil
+ * date : 2023/09/02
+ * description :
+ * =======================================================
+ * DATE          AUTHOR                      NOTE
+ * -------------------------------------------------------
+ * 2023/09/02        taeil                   최초생성
+ */
+public class ChannelInactive {
+    // channelInactive 이벤트는 channelActive 이벤트와는 반대로 채널이 비활성화되었을 때 발생한다.
+    // channelInactive 이벤트가 발생한 이후에는 채널에 대한 입출력 작업을 수행할 수 없다.
+}
