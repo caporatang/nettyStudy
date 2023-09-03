f4941757b7e26bdc2897864370fc37eedf68bb59
caporatang <caporatang@naver.com>
2023-09-03 23:34:51 +0900

    Docs : 4.3.2 bind 이벤트

---
+++ src/main/java/com/example/nettystudy/netty/chpater4/eventHnadler/channelOutboundEvent/Bind.java
@@ -0,0 +1,18 @@
+package com.example.nettystudy.netty.chpater4.eventHnadler.channelOutboundEvent;
+
+/**
+ * packageName : com.example.nettystudy.netty.chpater4.eventHnadler.channelOutboundEvent
+ * fileName : Bind
+ * author : taeil
+ * date : 2023/09/03
+ * description :
+ * =======================================================
+ * DATE          AUTHOR                      NOTE
+ * -------------------------------------------------------
+ * 2023/09/03        taeil                   최초생성
+ */
+public class Bind {
+    // 서버 소켓 채널이 클라이언트의 연결을 대기하는 IP와 포트가 설정되었을 때 발생한다.
+    // bind 이벤트에서는 서버 소켓 채널이 사용중인 SocketAddress 객체가 인수로 입력된다.
+    // 서버 소켓 채널이 사용하는 IP와 포트 정보를 확인할 수 있다.
+}
