71941493f6ad4806a22f433f847b340f601b882a
caporatang <caporatang@naver.com>
2023-09-04 22:38:08 +0900

    Feat : 4.6 사용자 정의 코덱

---
+++ src/main/java/com/example/nettystudy/netty/chpater4/codec/HttpHelloWorldServer.java
@@ -0,0 +1,79 @@
+package com.example.nettystudy.netty.chpater4.codec;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+import javax.net.ssl.SSLException;
+import java.security.cert.CertificateException;
+
+/**
+ * packageName : com.example.nettystudy.netty.chpater4.codec
+ * fileName : HttpHelloWorldServer
+ * author : taeil
+ * date : 2023/09/04
+ * description :
+ * =======================================================
+ * DATE          AUTHOR                      NOTE
+ * -------------------------------------------------------
+ * 2023/09/04        taeil                   최초생성
+ */
+public class HttpHelloWorldServer {
+    // 사용자 정의 코덱
+    // 네티 기본 제공 코덱은 인바운드와 아웃바운드 핸들러를 구현했다 -> 네트워크 입출력 프로토콜을 구현한 것이다.
+    // 사용자 정의 코덱은 사용자가 직접 필요한 프로토콜을 구현하는 것이다.
+    // 네티에서 기본 제공하는 HTTP 코덱을 사용하는 방법을 알아보자!
+
+
+    // 서버의 SSL 지원 여부를 설정한다.
+    static final boolean SSL = System.getProperty("ssl") != null;
+    static final int PORT = Integer.parseInt(System.getProperty("port", SSL ? "8443" : "8080"));
+
+    public static void main(String[] args) throws CertificateException, SSLException {
+        // Configure SSL.
+        final SslContext sslCtx;
+
+        if(SSL) {
+            SelfSignedCertificate ssc = new SelfSignedCertificate();
+            sslCtx = SslContext.newServerContext(ssc.certificate(), ssc.privateKey());
+            ssc.privateKey();
+        } else {
+            sslCtx = null;
+        }
+
+        //Configure the server
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.option(ChannelOption.SO_BACKLOG, 1024);
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    // 채널 파이프라인 설정
+                    .childHandler(new HttpHelloWorldServerInitializer(sslCtx));
+
+            Channel ch = b.bind(PORT).sync().channel();
+
+            System.out.println("Open your web browser and navigate to " + (SSL ? "https" : "http") + "://127.0.0.1:" + PORT + '/');
+            ch.closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
+}
---
+++ src/main/java/com/example/nettystudy/netty/chpater4/codec/HttpHelloWorldServerInitializer.java
@@ -0,0 +1,51 @@
+package com.example.nettystudy.netty.chpater4.codec;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.ssl.SslContext;
+
+/**
+ * packageName : com.example.nettystudy.netty.chpater4.codec
+ * fileName : HttpHelloWorldServerInitializer
+ * author : taeil
+ * date : 2023/09/04
+ * description :
+ * =======================================================
+ * DATE          AUTHOR                      NOTE
+ * -------------------------------------------------------
+ * 2023/09/04        taeil                   최초생성
+ */
+public class HttpHelloWorldServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    private final SslContext sslCtx;
+
+    public HttpHelloWorldServerInitializer(SslContext sslCtx) {
+        this.sslCtx = sslCtx;
+    }
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline p = ch.pipeline();
+        if(sslCtx != null) {
+            p.addLast(sslCtx.newHandler(ch.alloc()));
+        }
+
+        // 네티가 기본적으로 제공하는 HTTP 서버 코덱 : 인바운드와 아웃바운드 핸들러를 모두 구현한다.
+        // 즉, HttpServerCodec의 생성자에서 HttpRequestDecoder와 HttpResponseEncoder를 모두 생성한다.
+        // HttpServerCodec은 간단한 웹 서버를 생성하는 데 사용되는 코덱으로서 수신된 ByteBuf 객체를 HttpRequest와 HttpContent 객체로 변환하고 HttpResponse 객체를 ByteBuf로 인코딩하여 송신한다
+        p.addLast(new HttpServerCodec());
+
+        // HttpServerCodec이 수신한 이벤트와 데이터를 처리하여 HTTP 객체로 변환한 다음 channelRead 이벤틀르 HttpHelloWorldServerHandler 클래스로 전달한다.
+        p.addLast(new HttpHelloWorldServerHandler());
+
+
+
+        // ---------------- 순 서 중 요 -----------------
+        // 채널 파이프라인에 코덱과 핸들러의 순서를 반대로 설정하고 서버를 실행하면, 웹 서버는 정상적으로 실행되는데 이때 웹 브라우저를 사용하여 접속하면 아무런 응답도 받지 못한다.
+        // 채널로 데이터가 수신되면 가장 먼저 HttpHelloWorldServerHandler 이벤트 핸들러의 channelRead 이벤트 메서드가 수행되기 때문이다.
+        // 즉, HttpHelloWorldServerHandler의 channelRead 이벤트 메서드에서는 자신이 수신한 이벤트를 처리하고 나서 다음 이벤트 핸들러로 이벤트를 전달하지 않았기 떄문에 HttpServerCodec 이벤트 핸들러가 실행되지 않는다.
+    }
+}
