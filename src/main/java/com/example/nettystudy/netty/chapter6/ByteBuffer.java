56e29e98a87c853cb8b48deed8bb302ee6cca223
caporatang <caporatang@naver.com>
2023-09-12 22:44:21 +0900

    Docs : chapter6 ByteBuffer

---
+++ src/main/java/com/example/nettystudy/netty/chapter6/ByteBuffer.java
@@ -0,0 +1,18 @@
+package com.example.nettystudy.netty.chapter6;
+
+/**
+ * packageName : com.example.nettystudy.netty.chapter6
+ * fileName : ByteBuffer
+ * author : taeil
+ * date : 2023/09/12
+ * description :
+ * =======================================================
+ * DATE          AUTHOR                      NOTE
+ * -------------------------------------------------------
+ * 2023/09/12        taeil                   최초생성
+ */
+public class ByteBuffer {
+    // 자바 버퍼는 바이트 데이터를 저장하는 저장소로써 저장된 데이터를 읽고 쓰는 메서드를 제공한다.
+    // 또한 저장되는 데이터 형에 따라서 읽고 쓰는 데이터의 크기가 달라지는데 이를 Buffer 추상 클래스를 사용하여 처리한다.
+    // 자바가 버퍼 패키지를 제공함에도 불구하고 네티는 프레임워크 내부에서의 데이터 이동과 입출력에 자체 버퍼 API를 사용한다.
+}
